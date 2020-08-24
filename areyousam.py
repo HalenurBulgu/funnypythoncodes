5cb5f3cdf9ba340c09dd340f3dfec89247b2cae4
HalenurBulgu <38764127+HalenurBulgu@users.noreply.github.com>
2020-08-24 20:05:31 +0300

    Add files via upload

---
+++ areyousam.py
@@ -0,0 +1,10 @@
+name = 'Vampire'
+age = 4000
+if name == 'Sam':
+    print('Hi Sam')
+elif age < 10:
+    print('You are not Sam kid.')
+elif age>2000:
+    print('Unlike you, Sam is not an undead immortal vampire')
+elif age>100:
+    print(' you are not Sam, grannie..')
---
+++ meet.py
@@ -0,0 +1,11 @@
+#This program says hello and asks for my name
+
+print('hello world!')
+print('oo what is your name dear human?')
+myname = input()
+print('It is good to meet you, ' + myname)
+print('the length of your name is:')
+print(len(myname))
+print('What is your age?')
+myage = input()
+print('You will be ' + str(int(myage) + 3) + ' in three year.')
